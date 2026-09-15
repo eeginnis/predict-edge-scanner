4f261b1a806090656dc70760897523aa8805c072
eeginnis <elefterios.ginnis@gmail.com>
2026-09-14 20:43:36 -0400

    Update scanner to v3.2 with correct event status display
    Updates with correct event status display

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="predict-edge-v3.1";const A=["./","./index.html","./manifest.webmanifest","./icon.svg"];
+const C="predict-edge-v3.2";const A=["./","./index.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));
 self.addEventListener("fetch",e=>{if(e.request.url.includes("data-api.crypto.com"))return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
